b94fcc805ad8092fed6a881930b04ada9577a7ec
Taylor Robertson <taylorrobertson@Taylors-MacBook-Pro.local>
2021-09-20 14:15:50 -0400

    Create hook to manage game page form
    The useForm hook was created to allow the form state to be
    managed.

--- src/pages/game.js
+++ src/pages/game.js
@@ -2,6 +2,7 @@ import { graphql, Link } from "gatsby"
 import React, { useEffect, useState } from "react"
 import Layout from "../components/layout"
 import Nav from "../components/Nav"
+import useForm from "../hooks/useForm"
 import useRandomCountries from "../hooks/useRandomCountries"
 import "./game.modules.css";
 
@@ -13,33 +14,65 @@ const GamePage = ({ data }) => {
 
   useRandomCountries({ data, setAnswerOptions, setAnswer });
 
-  // Create logic for user to click button
-    // Should this be a form? 
+  const { values, updateValue } = useForm({
+    option: ''
+  })
 
-  console.log(answerOptions);
+  // Create submit button function/hook
+    // useSubmitAnswer
+    // compare selected answer with actual answer (answer)
 
+  // Abstract function into separate util library
+  function determineOptionLetter(index) {
+
+    const letters = ["A", "B", "C", "D"];
+
+    const letter = letters[index];
+
+    return `option${letter}`
+
+  }
+  
   return (
     <Layout>
       <Nav />
       <div className="game-page">
+        
+        <pre>{JSON.stringify(values, null, 2)}</pre>
+        
         <h2>Game</h2>
         
         <p>Choose the correct flag</p>
         <p>{answer?.name}</p>
-        <ul className="options-list">
-          {
-            answerOptions?.map(option => {
-              return (
-                <li key={option.numericCode}>
-                  <button>
-                    <img src={option.flag} alt="" />
-                    
-                  </button>
-                </li>
-              )
-            })
-          }
-        </ul>
+        <form>
+          <ul className="options-list">
+            {
+              answerOptions?.map((option, index) => {
+                const optionLetter = determineOptionLetter(index)
+
+                return (
+                  <li key={option.numericCode}>
+                    <img src={option.flag} alt={optionLetter} />
+                    <div>
+                      <label htmlFor={optionLetter}>
+                        <input 
+                          type="radio" 
+                          id={optionLetter}
+                          name="option" 
+                          value={optionLetter}
+                          checked={values.option === optionLetter} 
+                          onChange={updateValue}
+                        />
+                      </label>
+                    </div>
+                  </li>
+                )
+              })
+            }
+          </ul>
+          <button type="submit">Choose</button>
+        </form>
+
       </div>
     </Layout>
   )
